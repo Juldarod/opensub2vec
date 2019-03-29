0a1fb08a40980b08fa8c68e910bd77156c5ecf3b
Julian D. Rodriguez <juldarod10@hotmail.com>
2019-03-28 21:46:08 -0500

    First preprocess and training

---
+++ src/preprocess.py
@@ -0,0 +1,29 @@
+import re
+
+lines = 0
+
+input_path = '/home/julda/Documents/OpenSubtitles2018.en'
+output_path = '/home/julda/Documents/opensub2018.cor'
+# output1 = open('/home/julda/Documents/example.cor', 'w+')
+
+input = open(input_path, 'r')
+output = open(output_path, 'w+')
+# input.readlines()
+
+for line in input:
+    lines += 1
+    dash = re.sub(r'- ', '', line).lower()
+    noise = re.sub(r'[^a-zA-Z\'áéíóúÁÉÍÓÚ]+', ' ', dash).lower()
+    output.write(re.sub(r' \'', '\'', noise))
+    output.write('\n')
+
+# for i in range(50):
+#     lines += 1
+#     dash = re.sub(r'- ', '', input.readline()).lower()
+#     noise = re.sub(r'[^a-zA-Z\'áéíóúÁÉÍÓÚ]+', ' ', dash).lower()
+#     output1.write(re.sub(r' \'', '\'', noise))
+#     output1.write('\n')
+
+output.close()
+print('%s lines processed' % lines)
+# output1.close()
---
+++ src/training.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import logging
+from gensim.models.word2vec import LineSentence
+from gensim.utils import save_as_line_sentence
+from gensim.test.utils import get_tmpfile
+from gensim.models import Word2Vec
+
+# corpus = open('/home/julda/Documents/opensub2018.cor', 'r')
+# corpus_fname = get_tmpfile('/home/julda/Documents/opensub2018-line.cor')
+# save_as_line_sentence(corpus, corpus_fname)
+# corpus.close()
+# sentences = LineSentence(corpus)
+
+# path = get_tmpfile('opensub2018.model')
+# print(str(file))
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+model = Word2Vec(corpus_file=get_tmpfile('/home/julda/Documents/opensub2018.cor'), workers=4)
+model.save('opensub2018.model')
