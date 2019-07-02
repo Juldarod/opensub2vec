e11d1c3d21cc259e1f54d94c0e68c3bab073b9a4
Julian D. Rodriguez <juldarod10@hotmail.com>
2019-07-01 22:46:01 -0500

    Vector equivalence tests

--- opensub2vec/src/fasttext_tests.py
+++ opensub2vec/src/fasttext_tests.py
@@ -7,10 +7,17 @@ import logging
 root_path = "../resources/models/fasttext-pretrained/"
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-# en_vecs = fasttext.load_model(root_path + "cc.en.300.bin")
+en_vecs = fasttext.load_model(root_path + "cc.en.300.bin")
 # en_vecs = load_facebook_vectors(root_path + "cc.en.300.bin")
-en_vecs = KeyedVectors.load_word2vec_format(root_path + "cc.en.300.vec")
+# en_vecs = KeyedVectors.load_word2vec_format(root_path + "cc.en.300.vec")
 # en_vecs = FastTextKeyedVectors.load(root_path + "cc.en.300.vec", mmap='r')
 
+# en_vecs.save("../resources/models/fasttext/cc.en.300")
+
+en_vecs1 = KeyedVectors.load("../resources/models/fasttext/cc.en.300", mmap='r')
+
 # print(en_vecs['king'])
-print(en_vecs.most_similar('king'))
+# print(en_vecs1.get_vector('king'))
+print(en_vecs['king'] == en_vecs1.get_vector('king'))
+# print(en_vecs.most_similar_cosmul('man'))
+# print(en_vecs.most_similar_cosmul('woman'))
