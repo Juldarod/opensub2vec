ef8fb95694933045032bdaa831636bbe5e804562
Julian D. Rodriguez <juldarod10@hotmail.com>
2019-07-08 20:21:27 -0500

    Home html template

--- assets/js/components/Home.jsx
+++ assets/js/components/Home.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
-import { Container, Segment } from 'semantic-ui-react';
-import axios from 'axios';
+import React from "react";
+import { Container, Segment, Menu } from "semantic-ui-react";
+import axios from "axios";
 
 class Home extends React.Component {
-    state = {
-        backendStatus: '',
-    };
+  state = {
+    backendStatus: "",
+    activeItem: ""
+  };
 
-    componentDidMount() {
-        axios
-            .get('http://localhost:5000')
-            .then(response => {
-                console.log(response);
-                this.setState({ backendStatus: response.data.message });
-            })
-            .catch(error => console.log(error.response.data));
-    }
+  // componentDidMount() {
+  //     axios
+  //         .get('http://localhost:5000')
+  //         .then(response => {
+  //             console.log(response);
+  //             this.setState({ backendStatus: response.data.message });
+  //         })
+  //         .catch(error => console.log(error.response.data));
+  // }
 
-    render() {
-        return (
-            <Container id="hello">
-                <Segment>{this.state.backendStatus}</Segment>
-            </Container>
-        );
-    }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div id="home">
+        <div id="menu">
+          <label>Models</label>
+          <label>About</label>
+          <label>Contact</label>
+        </div>
+        <div id="landing">
+          <text>Opensub2Vec</text>
+          <div>
+            <div>
+              <label>Select a model</label>
+              <select />
+              <button>Go!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
